559c8e038a75147bfd995b61d9984adf30d98b43
yashsakhla <yashsakhla33@gmail.com>
2024-08-29 22:15:37 +0530

    added login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'; // Optional if you add icons
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatInputModule, MatButtonModule, MatCardModule, ReactiveFormsModule, MatIconModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-basic';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private renderer: Renderer2,
+    private el: ElementRef) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      console.log('Login successful', this.loginForm.value);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.typingEffect();
+  }
+
+  typingEffect() {
+    const phrases = ['Employee Onboarding System'];
+    const typingTextElement = this.el.nativeElement.querySelector(
+      '#typing-text'
+    );
+
+    if (typingTextElement) {
+      let textIndex = 0;
+
+      const type = () => {
+        if (textIndex < phrases[0].length) {
+          // Append each character to the element safely using Renderer2
+          const currentText = typingTextElement.innerHTML + phrases[0].charAt(textIndex);
+          this.renderer.setProperty(typingTextElement, 'innerHTML', currentText);
+          textIndex++;
+          setTimeout(type, 100);
+        }
+      };
+      type();
+    }
+  }
 }
